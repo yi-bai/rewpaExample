8bfec76e0b1b61544fcdf80b93eedb3362d8c3b2
Bai Yi <baiyi@team-lab.com>
2017-06-16 19:46:54 +0900

    WIP

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,15 +4,43 @@ import Actions from '../async/Actions';
 import { joinPath, getPath, createRewpa } from '../../../rewpa/src/index';
 import axios from 'axios';
 import _ from 'lodash';
+import Rx from 'rxjs';
 
 // Component
 class Counter extends React.Component {
     constructor(){
         super();
+        this.increment = this.increment.bind(this);
+        this.subject = new Rx.Subject();
+        this.another = this.subject.switchMap(() => Rx.Observable.fromPromise(this.increment()));
+        this.another.subscribe((promise) => console.log(promise));
+    }
+
+    increment(){
+      const { dispatch, path } = this.props;
+      return dispatch({ path, type: 'INCREMENT_EFFECTS' });
+    }
+
+    hehe(){
+      // this.subject.next(Math.random());
+      const someFunction = function(callback){
+        callback(1,2,3);
+      }
+
+      someFunction((a, b, c) => {
+        console.log(a); // 5
+        console.log(b); // 'some string'
+        console.log(c); // {someProperty: 'someValue'}
+      });
+
+      const boundSomeFunction = Rx.Observable.bindCallback(someFunction);
+      console.log(boundSomeFunction);
+      boundSomeFunction().subscribe(values => {
+        console.log(values) // [5, 'some string', {someProperty: 'someValue'}]
+      });
     }
 
     render(){
-      console.log(this.props);
       const { dispatch, path } = this.props;
       return (
             <div>
@@ -20,7 +48,7 @@ class Counter extends React.Component {
                 { this.props.count }
                 { this.props.isLoading ? 'loading...' : '' }
                 <span onClick={() => dispatch({ path, type: 'INCREMENT' })}>+</span>
-                <span onClick={() => dispatch({ path, type: 'INCREMENT_EFFECTS_A' })}> +ASYNC</span>
+                <span onClick={() => this.hehe()}> +ASYNC</span>
                 <span onClick={() => dispatch({ path, type: 'count/_SET', payload: 0 })}>Clear</span>
                 <span onClick={() => this.props.removeCounter()}>Remove</span>
                 { this.props.increment_called }, { this.props.throttle_called }
@@ -54,17 +82,17 @@ CounterContainer.rewpa = createRewpa({
       return dispatch({ path, type: 'INCREMENT_EFFECTS' })
       .then(() => dispatch({ path, type: 'INCREMENT_CALLED++' }));
     },
-    INCREMENT_EFFECTS: _.throttle(({ path }, dispatch, getState) => {
+    INCREMENT_EFFECTS: ({ path }, dispatch, getState) => {
       dispatch({ path, type: 'isLoading/_SET', payload: true });
       dispatch({ path, type: 'THROTTLE_CALLED++', payload: true });
       return axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response)
       .then((response) => {
-        console.log(response);
         dispatch({ path, type: 'INCREMENT' });
         dispatch({ path, type: 'isLoading/_SET', payload: false });
         return response.data.length;
       });
-    }, 10000)
+    }
   },
   reducer: {
     INCREMENT(state, action) { return _.assign({}, state, { count: state.count + (action.payload || 1) })  },
